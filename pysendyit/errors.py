ad952957243156c8de6e5354167dcdce0ad24f3d
0x6f736f646f <blackd0t@protonmail.com>
2020-05-25 21:18:48 +0300

    Adding BaseSendyException

--- pysendyit/errors.py
+++ pysendyit/errors.py
@@ -1,127 +1,74 @@
-class UnAuthorisedException(Exception):
+class BaseSendyException(Exception):
+    """
+    Base exception for all exceptions produced
+    """
+    def __init__(self, message, http_response):
+        self.message = message
+        self.status = http_response.status_code
+
+    @property
+    def get_message(self):
+        """
+        Returns this message
+        :returns message: str
+        """
+        return self.message
+
+
+class UnAuthorisedException(BaseSendyException):
     """
     Exception for requests that are being used without appropriate authorisation
     """
     def __init__(self, message, http_response):
-        super(UnAuthorisedException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(UnAuthorisedException, self).__init__(message=message, http_response=http_response)
 
 
-class MalformedRequestException(Exception):
+class MalformedRequestException(BaseSendyException):
     """
     Thrown if the request being sent is malformed or missing some paramters
     """
     def __init__(self, message, http_response):
-        super(MalformedRequestException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(MalformedRequestException, self).__init__(message=message, http_response=http_response)
 
 
-class NotFoundException(Exception):
+class NotFoundException(BaseSendyException):
     """
     Thrown if the request being sent is not available
     """
     def __init__(self, message, http_response):
-        super(NotFoundException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(NotFoundException, self).__init__(message=message, http_response=http_response)
 
 
-class UnAcceptableContentException(Exception):
+class UnAcceptableContentException(BaseSendyException):
     """
     Thrown if the request being sent has a header for a content type which isn't on the server
     """
     def __init__(self, message, http_response):
-        super(UnAcceptableContentException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(UnAcceptableContentException, self).__init__(message=message, http_response=http_response)
 
 
-class InvalidRequestException(Exception):
+class InvalidRequestException(BaseSendyException):
     """
     Thrown if the request being sent is parse-able but has invalid content
     """
     def __init__(self, message, http_response):
-        super(InvalidRequestException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(InvalidRequestException, self).__init__(message=message, http_response=http_response)
 
 
-class RateLimitException(Exception):
+class RateLimitException(BaseSendyException):
     """
     Thrown if the requests being sent are too many
     """
     def __init__(self, message, http_response):
-        super(RateLimitException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(RateLimitException, self).__init__(message=message, http_response=http_response)
 
 
-class ServerException(Exception):
+class ServerException(BaseSendyException):
     """
     Thrown if the there is internal server error
     """
     def __init__(self, message, http_response):
-        super(ServerException, self).__init__()
-        self.message = message
-        self.status = http_response.status
-
-    @property
-    def get_message(self):
-        """
-        Returns the message of the Un authorised error
-        :returns message: str
-        """
-        return self.message
+        super(ServerException, self).__init__(message=message, http_response=http_response)
 
 
 class SendyException(Exception):
@@ -135,7 +82,7 @@ class SendyException(Exception):
     @property
     def get_message(self):
         """
-        Returns the message of the Un authorised error
+        Returns this message
         :returns message: str
         """
         return self.message
