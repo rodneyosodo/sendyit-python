13419e50f66f50694ac85e12d6978ec5e74655e2
0x6f736f646f <blackd0t@protonmail.com>
2020-05-25 21:19:47 +0300

    Removing duplicates

--- pysendyit/pysendyit.py
+++ pysendyit/pysendyit.py
@@ -1,153 +1,90 @@
 from pysendyit.api import Api
 from pysendyit.errors import SendyException
-from pysendyit.check import *
+from pysendyit.check import (check_api_details, check_location_details,
+                             check_person_details, check_url, check_package_size,
+                             check_collect_payment, check_delivery_details)
 
 
 class Sendy(Api):
 
-    def __init__(self, api_key, api_username, base_url):
+    def __init__(self, api_key: str, api_username: str, base_url: str):
         """
-        `api_key` String api_key of the request eg: 'mysendykey'.
-        `api_username` String api_username of the request eg: 'mysendyusername'.
-        :param api_key:
-        :param api_username:
+        :param api_key: api_key of the request eg: 'mysendykey'.
+        :param api_username: api_username of the request eg: 'mysendyusername'.
         :param base_url:
         """
-        self.api_key = check_api_key(api_key)
-        self.api_username = check_api_username(api_username)
+        self.api_key = check_api_details(api_details=api_key)
+        self.api_username = check_api_details(api_details=api_username)
         self.base_url = check_url(base_url)
         super(Sendy, self).__init__(api_key, api_username, base_url)
 
     @staticmethod
-    def prepare_from_details(name, latitude, longitude, description):
+    def prepare_location_details(reference: str, name: str, latitude: float,
+                                 longitude: float, description: str) -> dict:
         """
-        Prepares data where you want the goods are, from location
-        :type name: str
-        :type latitude: float
-        :type longitude: float
-        :type description: str
-        :param name: from_name of the pick up location
-        :param latitude: from_latitude of the pick up location
-        :param longitude: from_longitude of the pick up location
-        :param description: from_description of the pick up location
-        :return from: dict
+        Prepares data for pickup location and destination of goods
+        :param reference: Either 'from' or 'to'
+        :param name: name of the pick up location or destination
+        :param latitude: latitude of the pick up location or destination
+        :param longitude: longitude of the pick up location or destination
+        :param description: description of the pick up location or destination
         """
-        return check_location_details({
-            "from_name": name,
-            "from_lat": latitude,
-            "from_long": longitude,
-            "from_description": description
-        })
+        if reference == "from" or reference == "to":
+            return check_location_details({
+                "{}_name".format(reference): name,
+                "{}_lat".format(reference): latitude,
+                "{}_long".format(reference): longitude,
+                "{}_description".format(reference): description
+            })
+        else:
+            raise SendyException("Invalid location reference")
 
     @staticmethod
-    def prepare_to_details(name, latitude, longitude, description):
+    def prepare_person_details(reference: str, name: str, phone: str, email: str, notes: str) -> dict:
         """
-        Prepares data where you want the goods are, from location
-        :type name: str
-        :type latitude: float
-        :type longitude: float
-        :type description: str
-        :param name: name of the destination
-        :param latitude: latitude of the destination
-        :param longitude: longitude of the destination
-        :param description: description of the destination
-        :return:
+        Prepares data of recepient or sender
+        :param reference: Either 'sender' or 'recepient'
+        :param name: name of the recepient or sender
+        :param phone: phone of the recepient or sender
+        :param email: email of the recepient or sender
+        :param notes: notes of the recepient or sender
         """
-        return check_location_details({
-            "to_name": name,
-            "to_lat": latitude,
-            "to_long": longitude,
-            "to_description": description
-        })
+        if reference == "recepient" or reference == "sender":
+            return check_person_details({
+                "{}_name".format("recepient"): name,
+                "{}_phone".format("recepient"): phone,
+                "{}_email".format("recepient"): email,
+                "{}_notes".format("recepient"): notes
+            })
+        else:
+            raise SendyException("Invalid reference for person. Give 'sender' or 'recepient'")
 
     @staticmethod
-    def prepare_recipient_details(name, phone, email, notes):
-        """
-        Prepares data of recepient
-        :type name: str
-        :type phone: str
-        :type email: str
-        :type notes: str
-        :param name: recepient_name of the recepient
-        :param phone: recepient_phone of the recepient
-        :param email: recepient_email of the recepient
-        :param notes: recepient_notes of the recepient
-        :return:
-        """
-        return check_person_details({
-            "recepient_name": name,
-            "recepient_phone": phone,
-            "recepient_email": email,
-            "recepient_notes": notes
-        })
-
-    @staticmethod
-    def prepare_sender_details(name, phone, email, notes):
-        """
-        Prepares data of sender
-        :type name: str
-        :type phone: str
-        :type email: str
-        :type notes: str
-        :param name: recepient_name of the recepient
-        :param phone: recepient_phone of the recepient
-        :param email: recepient_email of the recepient
-        :param notes: recepient_notes of the recepient
-        :return:
-        """
-        return check_person_details({
-            "sender_name": name,
-            "sender_phone": phone,
-            "sender_email": email,
-            "sender_notes": notes
-        })
-
-    @staticmethod
-    def prepare_package_size(weight, height, width, length, item_name):
+    def prepare_package_size(weight: float, height: float, width: float, length: float, item_name: str) -> dict:
         """
         Prepares data package size
-        :type weight: float
-        :type height: float
-        :type width: float
-        :type length: float
-        :type item_name: str
         :param weight: weight of the package
         :param height: height of the package
         :param width: width of the package
         :param length: length of the package
         :param item_name: item_name of the package
-        :return:
         """
-        return check_package_size({
-            "weight": weight,
-            "height": height,
-            "width": width,
-            "length": length,
-            "item_name": item_name
-        })
+        return check_package_size(dict(weight=weight, height=height, width=width, length=length, item_name=item_name))
 
     @staticmethod
-    def prepare_collect_payment(status, pay_method, amount):
+    def prepare_collect_payment(status: float, pay_method: float, amount: float) -> dict:
         """
         Prepares data package size
-        :type status: float
-        :type pay_method: float
-        :type amount: float
         :param status: weight of the package
         :param pay_method: height of the package
         :param amount: width of the package
-        :return:
         """
-        return check_collect_payment({
-            "status": status,
-            "pay_method": pay_method,
-            "amount": amount,
-        })
+        return check_collect_payment(dict(status=status, pay_method=pay_method, amount=amount))
 
     @staticmethod
     def prepare_delivery_details(pick_up_date, collect_payment, carrier_type, return_type,
                                  note, note_status, request_type, order_type, ecommerce_order,
-                                 express, skew, package_size):
+                                 express, skew, package_size) -> dict:
         return check_delivery_details({
             "pick_up_date": pick_up_date,
             "collect_payment": collect_payment,
@@ -253,36 +190,15 @@ class Sendy(Api):
         }
         return self.make_request(url_parameter=endpoint, body=values)
 
-    def track_delivery(self, order_no="AA34BE331", request_token_id="request_token_id"):
+    def track_or_cancel_delivery(self, command="track", order_no="AA2395374", request_token_id="request_token_id"):
         """
-        This call provides you with the ability to track current deliveries.
+        This call provides you with the ability to cancel current deliveries or track current deliveries.
+        :param command: Cancel or track the delivery
         :param order_no: status if money is being collected on your behalf
         :param request_token_id: request_token_id of the request
         :type order_no: str
         :type request_token_id: str
         """
-        command = "track"
-        endpoint = command
-        values = {
-            "command": command,
-            "data": {
-                "api_key": self.api_key,
-                "api_username": self.api_username,
-                "order_no": order_no
-            },
-            "request_token_id": request_token_id
-        }
-        return self.make_request(url_parameter=endpoint, body=values)
-
-    def cancel_delivery(self, order_no="AA2395374", request_token_id="request_token_id"):
-        """
-        This call provides you with the ability to cancel current deliveries.
-        :param order_no: status if money is being collected on your behalf
-        :param request_token_id: request_token_id of the request
-        :type order_no: str
-        :type request_token_id: str
-        """
-        command = "cancel"
         endpoint = command
         values = {
             "command": command,
@@ -302,9 +218,6 @@ class Sendy(Api):
         :param latitude: to_lat of the pick up location
         :param longitude: from_long of the pick up location
         :param request_token_id: request_token_id of the request
-        :type latitude: float
-        :type longitude: float
-        :type request_token_id: str
         """
         command = "rider_location"
         endpoint = "#rider"
