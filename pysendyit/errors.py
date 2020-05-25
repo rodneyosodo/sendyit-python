c43ec0c282c22f2c63bed18e30bd290a9aee230c
0x6f736f646f <blackd0t@protonmail.com>
2020-05-25 21:27:03 +0300

    Removing similar init

--- pysendyit/errors.py
+++ pysendyit/errors.py
@@ -5,6 +5,7 @@ class BaseSendyException(Exception):
     def __init__(self, message, http_response):
         self.message = message
         self.status = http_response.status_code
+        super(BaseSendyException, self).__init__()
 
     @property
     def get_message(self):
@@ -19,56 +20,42 @@ class UnAuthorisedException(BaseSendyException):
     """
     Exception for requests that are being used without appropriate authorisation
     """
-    def __init__(self, message, http_response):
-        super(UnAuthorisedException, self).__init__(message=message, http_response=http_response)
 
 
 class MalformedRequestException(BaseSendyException):
     """
     Thrown if the request being sent is malformed or missing some paramters
     """
-    def __init__(self, message, http_response):
-        super(MalformedRequestException, self).__init__(message=message, http_response=http_response)
 
 
 class NotFoundException(BaseSendyException):
     """
     Thrown if the request being sent is not available
     """
-    def __init__(self, message, http_response):
-        super(NotFoundException, self).__init__(message=message, http_response=http_response)
 
 
 class UnAcceptableContentException(BaseSendyException):
     """
     Thrown if the request being sent has a header for a content type which isn't on the server
     """
-    def __init__(self, message, http_response):
-        super(UnAcceptableContentException, self).__init__(message=message, http_response=http_response)
 
 
 class InvalidRequestException(BaseSendyException):
     """
     Thrown if the request being sent is parse-able but has invalid content
     """
-    def __init__(self, message, http_response):
-        super(InvalidRequestException, self).__init__(message=message, http_response=http_response)
 
 
 class RateLimitException(BaseSendyException):
     """
     Thrown if the requests being sent are too many
     """
-    def __init__(self, message, http_response):
-        super(RateLimitException, self).__init__(message=message, http_response=http_response)
 
 
 class ServerException(BaseSendyException):
     """
     Thrown if the there is internal server error
     """
-    def __init__(self, message, http_response):
-        super(ServerException, self).__init__(message=message, http_response=http_response)
 
 
 class SendyException(Exception):
